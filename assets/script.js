b8420fe914af548d3066d326e62346b981f06eae
Jim Mercurio <jamesm650@gmail.com>
2024-02-29 18:23:33 -0800

    JS and Button updates

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,30 @@ const questionEl = document.getElementById('question');
 const answerButtonEl = document.getElementById('possible-answer-buttons');
 
 
+let score;
+
+questions
+
+var questions = [
+    {
+    question: " What is   ",
+    answers:[ "Some","All" ,"None", "Any",  ],
+    correctAnswer: "None",
+    }
+]
+
+
+
+function startQuiz(){
+    score =0;
+    questionEl.style.display ='flex';
+
+
+
+
+}
+
+// Pull new question
+var currentQuestion = question[questionListMain]
+
+var questionText =document.getElementById('question')
